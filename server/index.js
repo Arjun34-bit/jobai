4a04ea70218d1fc2f2c69e759e35ac7d06aba54f
Arjun Devendra <arjundevendra34@gmail.com>
2025-05-21 19:14:59 +0530

    fix:CORS url fixed

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cors());
 app.use(
   cors({
-    origin: "https://jobai-k9mu.vercel.app/",
+    origin: "https://jobai-k9mu.vercel.app",
     credentials: true,
   })
 );
